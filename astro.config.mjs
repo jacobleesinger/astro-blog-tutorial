9394ae7527416f528f211ef32ff4c91fb632f39f
Jacob Singer <jacobleesinger@gmail.com>
2024-01-04 17:35:27 -0800

    feat: add greeting, dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fabulous-lokum-4b89c9.netlify.app/',
+  integrations: [preact()]
 });
